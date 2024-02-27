479692b4c15f83b7eaac27fc2facde73dfddd0a9
Suhas Lokesha <suhas.l.93@gmail.com>
2024-02-27 08:44:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <h1> Counter App </h1>
+      <p> Counter: {count} </p>
+      <button onClick={() => setCount((prev) => prev + 1)}> Increment </button>
+      <button onClick={() => setCount((prev) => prev - 1)}> Decrement </button>
+    </div>
+  );
+}
